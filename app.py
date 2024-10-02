76f77a31967560d7ffac24f09736b43b1ed8bf5a
Arun Mishra <arun484116@gmail.com>
2024-10-02 10:37:43 +0530

    another commit

--- app.py
+++ app.py
@@ -1,31 +1,51 @@
 import tensorflow as tf
-from tensorflow import keras 
-from tensorflow.keras.models import  load_model
-import streamlit as st 
-import numpy as np 
+from tensorflow.keras.models import load_model
+import streamlit as st
+import numpy as np
+from PIL import Image
 import sys
+
+# Reconfigure output for UTF-8 encoding
 sys.stdout.reconfigure(encoding='utf-8')
 
+# Streamlit header
+st.header('Image Classification Model')
 
-
-st.header('Image Classification Model ')
+# Load pre-trained model
 model = load_model('./Image_classify.keras')
-data_cat = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower', 'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 'pineapple', 'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 'sweetpotato', 'tomato', 'turnip', 'watermelon']
 
-image_height= 180
+# Class names
+data_cat = ['apple', 'banana', 'beetroot', 'bell pepper', 'cabbage', 'capsicum', 'carrot', 'cauliflower', 
+            'chilli pepper', 'corn', 'cucumber', 'eggplant', 'garlic', 'ginger', 'grapes', 'jalepeno', 
+            'kiwi', 'lemon', 'lettuce', 'mango', 'onion', 'orange', 'paprika', 'pear', 'peas', 
+            'pineapple', 'pomegranate', 'potato', 'raddish', 'soy beans', 'spinach', 'sweetcorn', 
+            'sweetpotato', 'tomato', 'turnip', 'watermelon']
+
+# Set image dimensions
+image_height = 180
 image_width = 180
-image = st.text_input('Enter Image Name','Image.jpg')
 
-image_load = tf.keras.utils.load_img(image,target_size=(image_height,image_width))
-img_arr =tf.keras.utils.array_to_img(image_load)
-img_bat=tf.expand_dims(img_arr,0)
+# Drag-and-drop image upload
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-predict = model.predict(img_bat)
+if uploaded_file is not None:
+    # Load and preprocess image
+    image = Image.open(uploaded_file)
+    image = image.resize((image_height, image_width))  # Resize image for the model
+    
+    # Convert image to array
+    img_array = tf.keras.utils.img_to_array(image)
+    
+    # Expand dimensions for batch processing (model expects batch size as the first dimension)
+    img_bat = tf.expand_dims(img_array, 0)
 
-score = tf.nn.softmax(predict)  
-st.image(image,width = 200)
-st.write('Veg/Fruit in image is ' + data_cat[np.argmax(score)])
-st.write('with accuracy of ' + str(np.max(score)*100))
+    # Make prediction
+    predictions = model.predict(img_bat)
+    score = tf.nn.softmax(predictions[0])
 
-
-
+    # Display uploaded image
+    st.image(image, caption="Uploaded Image", width=200)
+    
+    # Show prediction and accuracy
+    st.write(f'Prediction: **{data_cat[np.argmax(score)]}**')
+    st.write(f'Confidence: **{np.max(score) * 100:.2f}%**')
